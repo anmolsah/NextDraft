3808b7efebe2b5fa2a2a2e8de87668d8d7f7c0bf
anmolsah <kumarxxx242424@gmail.com>
2025-12-18 20:25:02 +0530

    feat: add project fetching logic and loading state to MyProjects component

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import type { Project } from "../types";
 
 const MyProjects = () => {
-  const [loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const fetchProjects = async () => {};
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="px-4 md:px-16 lg:px-24 xl:px-32">
+        {loading ? <div> </div> : projects.length > 0 (<div> </div>)}
+      </div>
+    </>
   );
 };
 
