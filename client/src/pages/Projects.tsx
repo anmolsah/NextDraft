94cfab40011f161f2a8cfbd618d926a64ebe948b
anmolsah <kumarxxx242424@gmail.com>
2026-01-02 20:38:04 +0530

    feat(projects): enhance Projects page with project fetching and loading state

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import type { Project } from "../types";
 import { Loader2Icon } from "lucide-react";
+import { dummyConversations, dummyProjects } from "../assets/assets";
 
 const Projects = () => {
   const { projectId } = useParams();
   const navigate = useNavigate();
 
   const [project, setProject] = useState<Project | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [isGenerating, setIsGenerating] = useState(true);
   const [device, setDevice] = useState<"phone" | "tablet" | "desktop">(
     "desktop"
@@ -16,7 +17,16 @@ const Projects = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
 
-  const fetchProject = async () => {};
+  const fetchProject = async () => {
+    const project = dummyProjects.find((project) => project.id === projectId);
+    setTimeout(() => {
+      if (project) {
+        setProject({ ...project, conversation: dummyConversations });
+        setLoading(false);
+        setIsGenerating(project.current_code ? false : true);
+      }
+    }, 2000);
+  };
 
   useEffect(() => {
     fetchProject();
@@ -33,7 +43,18 @@ const Projects = () => {
   }
 
   return project ? (
-    <div></div>
+    <div className="flex flex-col h-screen w-full bg-gray-900 text-white">
+      <div className="flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar">
+        {/* left */}
+        <div>
+          <img src="" alt="" />
+        </div>
+        {/* middle */}
+        <div></div>
+        {/* right */}
+        <div></div>
+      </div>
+    </div>
   ) : (
     <div className="flex items-center justify-center h-screen">
       <p className="text-2xl font-medium text-[#B8BBC7]">
